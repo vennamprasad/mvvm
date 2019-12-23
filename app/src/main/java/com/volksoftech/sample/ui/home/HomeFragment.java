71cd09dd604c4d90c8d3d2c7191eb8ffe927e9ca
vennam prasad <vennamprasad@gmail.com>
2019-12-23 16:42:09 +0530

    vs

--- app/src/main/java/com/volksoftech/sample/ui/home/HomeFragment.java
+++ app/src/main/java/com/volksoftech/sample/ui/home/HomeFragment.java
@@ -16,12 +16,9 @@ import com.volksoftech.sample.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
@@ -32,4 +29,5 @@ public class HomeFragment extends Fragment {
         });
         return root;
     }
+
 }
